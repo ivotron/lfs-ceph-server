eeb78e3f6e141601f779fcb288d0b81029319a30
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-04-12 11:17:13 -0700

    WIP, need to test

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,21 @@
-FROM golang:1.4-onbuild
-MAINTAINER GitHub, Inc.
+FROM golang:1.6
+
+MAINTAINER Ivo Jimenez <ivo.jimenez@gmail.com>
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends  librados-dev && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN mkdir -p /go/src/app
+WORKDIR /go/src/app
+
+ADD . /go/src/app
+RUN go-wrapper download
+RUN go-wrapper install
+
+ADD ceph.conf /root
+
 EXPOSE 8080
+EXPOSE 9999
+
+CMD ["go-wrapper", "run"]
--- content_store.go
+++ content_store.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
+	"bytes"
 	"errors"
 	"io"
-	"os"
-	"path/filepath"
+
+	"github.com/ceph/go-ceph/rados"
 )
 
 var (
@@ -16,62 +15,50 @@ var (
 
 // ContentStore provides a simple file system based storage.
 type ContentStore struct {
-	basePath string
+	conn  *rados.Conn
+	ioctx *rados.IOContext
 }
 
 // NewContentStore creates a ContentStore at the base directory.
 func NewContentStore(base string) (*ContentStore, error) {
-	if err := os.MkdirAll(base, 0750); err != nil {
+	conn, err := rados.NewConn()
+	if err != nil {
+		return nil, err
+	}
+	conn.ReadConfigFile("/etc/ceph/ceph.conf")
+	conn.Connect()
+	ioctx, err := conn.OpenIOContext("data")
+	if err != nil {
 		return nil, err
 	}
 
-	return &ContentStore{base}, nil
+	return &ContentStore{conn, ioctx}, nil
 }
 
 // Get takes a Meta object and retreives the content from the store, returning
 // it as an io.Reader.
 func (s *ContentStore) Get(meta *MetaObject) (io.Reader, error) {
-	path := filepath.Join(s.basePath, transformKey(meta.Oid))
-
-	return os.Open(path)
+	stat, err := s.ioctx.Stat(meta.Oid)
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, stat.Size)
+	bytesRead, err := s.ioctx.Read(meta.Oid, buf, 0)
+	if err != nil {
+		return nil, err
+	}
+	if uint64(bytesRead) != stat.Size {
+		return nil, errSizeMismatch
+	}
+	return bytes.NewReader(buf), nil
 }
 
 // Put takes a Meta object and an io.Reader and writes the content to the store.
 func (s *ContentStore) Put(meta *MetaObject, r io.Reader) error {
-	path := filepath.Join(s.basePath, transformKey(meta.Oid))
-	tmpPath := path + ".tmp"
-
-	dir := filepath.Dir(path)
-	if err := os.MkdirAll(dir, 0750); err != nil {
-		return err
-	}
-
-	file, err := os.OpenFile(tmpPath, os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0640)
-	if err != nil {
-		return err
-	}
-	defer os.Remove(tmpPath)
-
-	hash := sha256.New()
-	hw := io.MultiWriter(hash, file)
-
-	written, err := io.Copy(hw, r)
-	if err != nil {
-		file.Close()
-		return err
-	}
-	file.Close()
-
-	if written != meta.Size {
-		return errSizeMismatch
-	}
-
-	shaStr := hex.EncodeToString(hash.Sum(nil))
-	if shaStr != meta.Oid {
-		return errHashMismatch
-	}
-
-	if err := os.Rename(tmpPath, path); err != nil {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r)
+	buf.Bytes()
+	if err := s.ioctx.Write(meta.Oid, buf.Bytes(), 0); err != nil {
 		return err
 	}
 	return nil
@@ -79,17 +66,13 @@ func (s *ContentStore) Put(meta *MetaObject, r io.Reader) error {
 
 // Exists returns true if the object exists in the content store.
 func (s *ContentStore) Exists(meta *MetaObject) bool {
-	path := filepath.Join(s.basePath, transformKey(meta.Oid))
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return false
+	if _, err := s.ioctx.Stat(meta.Oid); err != nil {
+		if err == rados.RadosErrorNotFound {
+			return false
+		} else {
+			panic(err)
+		}
+	} else {
+		return true
 	}
-	return true
-}
-
-func transformKey(key string) string {
-	if len(key) < 5 {
-		return key
-	}
-
-	return filepath.Join(key[0:2], key[2:4], key[4:len(key)])
 }
